4835ab704e0ccdcaf828b6001e062f961bb34173
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2025-04-11 09:30:11 +0500

    Update Bcard.js

--- src/button/bootstrap/Bcard.js
+++ src/button/bootstrap/Bcard.js
@@ -2,10 +2,11 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import imageBurger from "../../../asset/pizza.png"
 
+// Carrd 
 function BootCard() {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={imageBurger} width={100} height={260}/>
+    <Card style={{ width: '20rem' }}>
+      <Card.Img variant="top" src={pizza} width={100} height={200}/>
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
@@ -15,6 +16,112 @@ function BootCard() {
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
+  );
+}
+
+
+// From Text 
+
+<div>
+function FormTextExample() {
+  return (
+    <>
+      <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+      <Form.Control
+        type="password"
+        id="inputPassword5"
+        aria-describedby="passwordHelpBlock"
+      />
+      <Form.Text id="passwordHelpBlock" muted>
+        Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji.
+      </Form.Text>
+    </>
+  );
+}
+</div>
+
+
+// Input Gropu
+<div>
+function BasicExample() {
+  return (
+    <>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+        <Form.Control
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+      </InputGroup>
+
+      <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon3">
+          https://example.com/users/
+        </InputGroup.Text>
+        <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <InputGroup.Text>$</InputGroup.Text>
+        <Form.Control aria-label="Amount (to the nearest dollar)" />
+        <InputGroup.Text>.00</InputGroup.Text>
+      </InputGroup>
+
+      <InputGroup>
+        <InputGroup.Text>With textarea</InputGroup.Text>
+        <Form.Control as="textarea" aria-label="With textarea" />
+      </InputGroup>
+    </>
+  );
+}
+</div>
+
+// Tables
+import Table from 'react-bootstrap/Table';
+
+function BasicExample() {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
 
